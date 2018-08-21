43bd6691c6b442045cf09e75db91772c5fb557a2
haojixiang <hao_jixiang@163.com>
2018-08-21 10:16:39 +0800

    观察者模式

---
+++ src/main/java/com/ralap/design/design/observe/BossSubject.java
@@ -0,0 +1,9 @@
+package com.ralap.design.design.observe;
+
+/**
+ * @author: ralap
+ * @date: created at 2018/8/21 10:14
+ */
+public class BossSubject {
+
+}
---
+++ src/main/java/com/ralap/design/design/observe/NBAObserve.java
@@ -0,0 +1,12 @@
+package com.ralap.design.design.observe;
+
+import java.util.Observable;
+
+/**
+ * @author: ralap
+ * @date: created at 2018/8/21 10:13
+ */
+public class NBAObserve extends Observable {
+
+
+}
